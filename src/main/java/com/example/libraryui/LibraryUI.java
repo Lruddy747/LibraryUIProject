8030b6b9633a7097633c70948dbc312e3fdbc2ed
Lruddy747 <liampruddy@gmail.com>
2024-10-10 14:12:55 -0400

    Initial commit

---
+++ src/main/java/com/example/libraryui/LibraryUI.java
@@ -0,0 +1,31 @@
+package com.example.libraryui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.sql.*;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
